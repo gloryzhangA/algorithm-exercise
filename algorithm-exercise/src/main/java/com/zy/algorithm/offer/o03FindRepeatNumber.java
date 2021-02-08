b363117e47bafc175f398434b4d535ff1c33f6f3
gloryzhangA <635905143@qq.com>
2021-02-08 23:51:22 +0800

    完成剑指offer第三题

---
+++ algorithm-exercise/src/main/java/com/zy/algorithm/offer/o03FindRepeatNumber.java
@@ -0,0 +1,24 @@
+package com.zy.algorithm.offer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class o03FindRepeatNumber {
+
+    public static int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        int repeat = -1;
+        for (int num : nums) {
+            if (!set.add(num)) {
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 0, 2, 5, 3};
+        System.out.println(findRepeatNumber(nums));
+    }
+}
